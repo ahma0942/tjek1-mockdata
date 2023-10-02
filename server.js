04dac0212966d1627b4e8b557f36fdf1bae68978
Ahmad Almajedi <kontakt@ahmad.nu>
2023-10-02 06:10:34 +0200

    Fix endpoint

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
   if (reqUrl === "/CPR") {
     res.write(fs.readFileSync("data/CPR.json"));
   }
-  if (reqUrl === "/CPR/CprPersonFullSimple/1/rest/PersonFullListSimple" && parsed.query["pnr.personnummer.eq"]) {
+  if (reqUrl === "/CPR/CprPersonFullComplete/1/rest/PersonFullCurrentListComplete" && parsed.query["pnr.personnummer.eq"]) {
     const cpr = parsed.query["pnr.personnummer.eq"];
     const json = JSON.parse(fs.readFileSync("data/CPR.json"));
     const person = json.Personer.find((p) => p.Person.Personnumre.find((pnr) => pnr.Personnummer.personnummer === cpr));
