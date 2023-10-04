d86c6b90fb4596d2fe41fccb50ccee08b146cac8
Ahmad Almajedi <kontakt@ahmad.nu>
2023-10-04 23:26:18 +0200

    Fix mockdata server to match data from CPR

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
     const json = JSON.parse(fs.readFileSync("data/CPR.json"));
     const person = json.Personer.find((p) => p.Person.Personnumre.find((pnr) => pnr.Personnummer.personnummer === cpr));
     if (person) {
-      res.write(JSON.stringify(person));
+      res.write(JSON.stringify({ Personer: [person] }));
     } else {
       res.writeHead(404);
     }
@@ -27,14 +27,14 @@ const server = http.createServer((req, res) => {
   if (reqUrl.toLowerCase() === "/cvr/n" && parsed.query["pnr.cvr.eq"]) {
     const cvr = parsed.query["pnr.cvr.eq"].toString();
     const json = JSON.parse(fs.readFileSync("data/CVR.json"));
-    const filteredObjects = json.filter(item => item.virksomhed.CVRNummer.toString() === cvr);
-    
+    const filteredObjects = json.filter((item) => item.virksomhed.CVRNummer.toString() === cvr);
+
     if (filteredObjects.length > 0) {
-      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.writeHead(200, { "Content-Type": "application/json" });
       res.write(JSON.stringify(filteredObjects));
     } else {
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.write('Not Found');
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.write("Not Found");
     }
     res.end();
   }
